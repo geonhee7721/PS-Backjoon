6519c37e47d527e6b59bf446548111e4035106f0
geonhee7721 <76245227+geonhee7721@users.noreply.github.com>
2021-03-07 23:33:22 +0900

    Update 11279_최대힙.cpp

--- PS-Backjoon/PS-Backjoon/11279_최대힙.cpp
+++ PS-Backjoon/PS-Backjoon/11279_최대힙.cpp
@@ -1,24 +1,45 @@
 ﻿#include <stdio.h>
 
+
 int heap[100001];
 int N;
 int heapSize = 0;
 
-void heapPush(int value){
+void heapPush(int value)
+{
 	heap[heapSize] = value;
-	int current = heapSize;	while (current > 0 && heap[current] > heap[(current - 1) / 2]) {		int temp = heap[(current - 1) / 2];		heap[(current - 1) / 2] = heap[current];		heap[current] = temp;		current = (current - 1) / 2;	}
-	heapSize = heapSize + 1;}
+	int current = heapSize;
+	while (current > 0 && heap[current] > heap[(current - 1) / 2]) {
+		int temp = heap[(current - 1) / 2];
+		heap[(current - 1) / 2] = heap[current];
+		heap[current] = temp;
+		current = (current - 1) / 2;
+	}
+	heapSize = heapSize + 1;
+}
 
-int heapPop(){	if (heapSize <= 0) return 0;
+int heapPop()
+{
+	if (heapSize <= 0) return 0;
 
-	int value = heap[0];	heapSize = heapSize - 1;
+	int value = heap[0];
+	heapSize = heapSize - 1;
 	heap[0] = heap[heapSize];
-	int current = 0;	while (current * 2 + 1 < heapSize) {		int child;		if (current * 2 + 2 == heapSize) child = current * 2 + 1;		else child = heap[current * 2 + 1] > heap[current * 2 + 2] ? current * 2 + 1 : current * 2 + 2;
+	int current = 0;
+	while (current * 2 + 1 < heapSize) {
+		int child;
+		if (current * 2 + 2 == heapSize) child = current * 2 + 1;
+		else child = heap[current * 2 + 1] > heap[current * 2 + 2] ? current * 2 + 1 : current * 2 + 2;
 
 		if (heap[current] > heap[child]) break;
 
-		int temp = heap[current];		heap[current] = heap[child];		heap[child] = temp;
-		current = child;	}	return value;}
+		int temp = heap[current];
+		heap[current] = heap[child];
+		heap[child] = temp;
+		current = child;
+	}
+	return value;
+}
 
 int main() {
 	scanf("%d", &N);
